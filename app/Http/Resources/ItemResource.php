66a387579406408aa7531cf70557c29c18924ce5
danielspinard <d.spinardi199@gmail.com>
2021-06-06 23:36:39 -0300

    Fix JsonSerializable toArray method in ItemResource

--- app/Http/Resources/ItemResource.php
+++ app/Http/Resources/ItemResource.php
@@ -7,11 +7,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class ItemResource extends JsonResource
 {
     /**
-     * @param \Illuminate\Http\Request $request
+     * @param Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'completed' => $this->completed,
+            'completed_at' => $this->completed_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
