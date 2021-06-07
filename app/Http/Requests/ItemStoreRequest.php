48592c300ef484129f3a6535262ecc5d2cb1c3d2
danielspinard <d.spinardi199@gmail.com>
2021-06-07 00:04:58 -0300

    Fix ItemStoreRequest authorize and rules methods

--- app/Http/Requests/ItemStoreRequest.php
+++ app/Http/Requests/ItemStoreRequest.php
@@ -7,24 +7,20 @@ use Illuminate\Foundation\Http\FormRequest;
 class ItemStoreRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
-            //
+            'name' => 'required|alpha|min:3|max:15',
         ];
     }
 }
