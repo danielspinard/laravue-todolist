62f0b3bd5236718c775ec8b6c6cb1a38a3c55425
danielspinard <d.spinardi199@gmail.com>
2021-06-06 23:48:51 -0300

    Fix api order routes

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::get('/items', [ItemController::class, 'index']);
 Route::prefix('/item')->group(function () {
     Route::post('/store', [ItemController::class, 'store']);
     Route::get('/{id}', [ItemController::class, 'show']);
-    Route::delete('/{id}', [ItemController::class, 'destroy']);
     Route::put('/{id}', [ItemController::class, 'update']);
+    Route::delete('/{id}', [ItemController::class, 'destroy']);
 });
