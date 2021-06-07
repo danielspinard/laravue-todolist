9f721f4d7a0b5c42d5046f5fb386a14f07548904
danielspinard <d.spinardi199@gmail.com>
2021-06-06 23:27:46 -0300

    Fix ItemModel fillable property

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'completed', 'completed_at'
+    ];
 }
