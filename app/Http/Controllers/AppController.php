37cfd109414db787bd2b06468331d764b1c94d5a
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-09-06 13:08:14 -0300

    Add method to call Inertia render method

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
-    //
+    /**
+     * @return Inertia\Response
+     */
+    public function web(): Inertia\Response
+    {
+        return Inertia::render('App');
+    }
 }
